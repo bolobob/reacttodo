8d858daf7d5925f3205435b52af715ecdb3be07c
bolobob <aokishinichi@gmail.com>
2020-10-03 21:51:19 +0900

    ヘッダとタイトルを設置

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React, { FC } from 'react';
 import Container from '@material-ui/core/Container';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
 
 const App: FC = () => {
   return (
     <Container maxWidth="sm" disableGutters>
+      <AppBar position="sticky">
+        <Typography variant="h6" component="h1" align="center">ToDo</Typography>
+      </AppBar>
     </Container>
   );
 }
