be329e322d36f22140ee842747bf8b0f19ba18a6
bolobob <aokishinichi@gmail.com>
2020-10-29 01:21:10 +0900

    refactor: prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
-import React, { FC, useEffect, useState, useRef, FormEvent, ChangeEvent } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import dayjs from 'dayjs';
-import db from './db';
-import Container from '@material-ui/core/Container';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import InputBase from '@material-ui/core/InputBase';
-import Checkbox from '@material-ui/core/Checkbox';
-import IconButton from '@material-ui/core/IconButton';
-import AddIcon from '@material-ui/icons/AddCircle';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import DeleteIcon from '@material-ui/icons/Delete';
+import React, {
+  FC,
+  useEffect,
+  useState,
+  useRef,
+  FormEvent,
+  ChangeEvent,
+} from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import dayjs from "dayjs";
+import db from "./db";
+import Container from "@material-ui/core/Container";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import InputBase from "@material-ui/core/InputBase";
+import Checkbox from "@material-ui/core/Checkbox";
+import IconButton from "@material-ui/core/IconButton";
+import AddIcon from "@material-ui/icons/AddCircle";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 type Task = {
   id?: number;
@@ -27,19 +34,19 @@ type Task = {
 
 const useStyles = makeStyles({
   toolBar: {
-    justifyContent: 'space-between'
+    justifyContent: "space-between",
   },
   addIcon: {
-    position: 'absolute',
-    right: '10px',
-    bottom: '10px'
+    position: "absolute",
+    right: "10px",
+    bottom: "10px",
   },
   hidden: {
-    display: 'none'
-  }
+    display: "none",
+  },
 });
 
-dayjs.locale('ja');
+dayjs.locale("ja");
 
 const App: FC = () => {
   const classes = useStyles();
@@ -49,9 +56,11 @@ const App: FC = () => {
   const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
-    db.table('todos').toArray().then((tasks) => {
-      setTasks(tasks);
-    });
+    db.table("todos")
+      .toArray()
+      .then((tasks) => {
+        setTasks(tasks);
+      });
   }, []);
 
   useEffect(() => {
@@ -63,17 +72,22 @@ const App: FC = () => {
   };
 
   const addTask = (task: Task) => {
-    db.table('todos').put({
-      completed: task.completed,
-      content: task.content,
-      created_at: dayjs().format(),
-      updated_at: dayjs().format(),
-    }).then(() => {
-      setTasks((tasks) => [task, ...tasks]);
-    });
+    db.table("todos")
+      .put({
+        completed: task.completed,
+        content: task.content,
+        created_at: dayjs().format(),
+        updated_at: dayjs().format(),
+      })
+      .then(() => {
+        setTasks((tasks) => [task, ...tasks]);
+      });
   };
 
-  const toggleCompleted = (task: Task) => (e: ChangeEvent<HTMLInputElement>, checked: boolean) => {
+  const toggleCompleted = (task: Task) => (
+    e: ChangeEvent<HTMLInputElement>,
+    checked: boolean
+  ) => {
     setTasks((tasks) => {
       return tasks.map((t) => {
         if (t.id === task.id) {
@@ -86,31 +100,33 @@ const App: FC = () => {
 
   const createTask = (e: FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
-    addTask({ completed: false, content: inputRef.current?.value ?? ''});
+    addTask({ completed: false, content: inputRef.current?.value ?? "" });
     if (inputRef.current !== null) {
-      inputRef.current.value = '';
+      inputRef.current.value = "";
     }
     setNewFormShown(false);
-  }
+  };
 
   return (
     <Container maxWidth="sm" disableGutters>
       <AppBar position="sticky">
         <Toolbar className={classes.toolBar}>
-          <Typography variant="h6" component="h1" align="center">ToDo</Typography>
+          <Typography variant="h6" component="h1" align="center">
+            ToDo
+          </Typography>
           <Button color="inherit">編集</Button>
         </Toolbar>
       </AppBar>
       <List>
-        <ListItem className={newFormShown ? '' : classes.hidden} dense>
+        <ListItem className={newFormShown ? "" : classes.hidden} dense>
           <ListItemIcon>
-          <Checkbox
-            edge="start"
-            checked={false}
-            tabIndex={-1}
-            disableRipple
-            inputProps={{ 'aria-labelledby': labelId }}
-          />
+            <Checkbox
+              edge="start"
+              checked={false}
+              tabIndex={-1}
+              disableRipple
+              inputProps={{ "aria-labelledby": labelId }}
+            />
           </ListItemIcon>
           <form noValidate autoComplete="off" onSubmit={createTask}>
             <InputBase id="standard-basic" inputRef={inputRef} />
@@ -119,14 +135,14 @@ const App: FC = () => {
         {tasks.map((task) => (
           <ListItem key={task.id} dense>
             <ListItemIcon>
-            <Checkbox
-              edge="start"
-              checked={task.completed}
-              tabIndex={-1}
-              disableRipple
-              inputProps={{ 'aria-labelledby': labelId }}
-              onChange={toggleCompleted(task)}
-            />
+              <Checkbox
+                edge="start"
+                checked={task.completed}
+                tabIndex={-1}
+                disableRipple
+                inputProps={{ "aria-labelledby": labelId }}
+                onChange={toggleCompleted(task)}
+              />
             </ListItemIcon>
             <form noValidate autoComplete="off">
               <InputBase value={task.content} />
@@ -139,11 +155,15 @@ const App: FC = () => {
           </ListItem>
         ))}
       </List>
-      <IconButton className={classes.addIcon} aria-label="delete" onClick={showForm}>
+      <IconButton
+        className={classes.addIcon}
+        aria-label="delete"
+        onClick={showForm}
+      >
         <AddIcon />
       </IconButton>
     </Container>
   );
-}
+};
 
 export default App;
