d77e302ba75c0d72639be8d1f2907d066b476789
bolobob <aokishinichi@gmail.com>
2020-11-23 20:04:50 +0900

    イベントハンドラはactionで定義する

--- src/components/organisms/Header.story.tsx
+++ src/components/organisms/Header.story.tsx
@@ -1,10 +1,16 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/core";
 import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
 import { boolean } from "@storybook/addon-knobs";
 
 import Header from "./Header";
 
-storiesOf('Common/Header', module).add('default', () => {
-  return (<Header editing={boolean('editing', false)} toggleEditing={()=>{}}/>);
+storiesOf("Common/Header", module).add("default", () => {
+  return (
+    <Header
+      editing={boolean("editing", false)}
+      toggleEditing={action("clicked")}
+    />
+  );
 });
