442fcceaf8c8b0fc0593dce04df82584871cf1ad
bolobob <aokishinichi@gmail.com>
2020-11-23 18:15:54 +0900

    アドオンの設定

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,14 @@
 import { configure, addDecorator } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import { withKnobs } from '@storybook/addon-knobs';
 
 import '../src/index.css';
 
 const req = require.context('../src/components', true, /.(story|stories).tsx$/);
 
 function loadStories() {
+  addDecorator(withInfo);
+  addDecorator(withKnobs);
   req.keys().forEach(req);
 }
 
